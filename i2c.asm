3b82677ccd6581b8461787b910c0ca70e3b268e5
stefan-b-jakobsson <stefan.b.jakobsson@gmail.com>
2023-03-18 20:40:03 +0200

    Upload of partially working I2C code so I can revert if need be

--- i2c.asm
+++ i2c.asm
@@ -1,34 +1,56 @@
-.equ I2C_SLAVE_ADDRESS = 0x42
+; Slave address
+.equ I2C_SLAVE_ADDRESS      = 0x44
 
-.equ I2C_CLK           = 2
-.equ I2C_SDA           = 0
+; Pins
+.equ I2C_CLK                = 2
+.equ I2C_SDA                = 0
 
-.equ I2C_IDLE          = 0x00
-.equ I2C_CHECK_ADDRESS = 0x01
-.equ I2C_ADDRESS_ACK   = 0x02
-.equ I2C_ADDRESS_NACK  = 0x03
-.equ I2C_RECEIVE       = 0x04
-.equ I2C_RECEIVE_ACK   = 0x05
-.equ I2C_RECEIVE_NACK  = 0x06
-.equ I2C_SEND          = 0x07
-.equ I2C_SEND_WAIT     = 0x08
+; I2C states
+.equ STATE_CHECK_ADDRESS    = 0x01
+.equ STATE_WAIT_SLAVE_ACK   = 0x02
+.equ STATE_RECEIVE_BYTE     = 0x03
+.equ STATE_WAIT_SLAVE_NACK  = 0x04
+.equ STATE_TRANSMIT_BYTE    = 0x05
+.equ STATE_PREP_MASTER_ACK  = 0x06
+.equ STATE_WAIT_MASTER_ACK  = 0x07
 
+.equ I2C_RECEIVE            = 0x00
+.equ I2C_SEND               = 0x01
+
+; USICR values
+.equ I2C_CR_IDLE            = 0b10101000
+.equ I2C_CR_STOP            = 0b10111000
+.equ I2C_CR_ACTIVE          = 0b11101000
+
+; USISR values
+.equ I2C_SR_BYTE            = 0xf0
+.equ I2C_SR_BIT             = 0xf0 | 0x0e
+
+;******************************************************************************
+; Data segment
+;******************************************************************************
 .dseg
 
 i2c_state: .byte 1
-i2c_addr: .byte 1
+i2c_offset: .byte 1
+i2c_ddr: .byte 1
+i2c_echo_buf: .byte 1
 
-heartbeat: .byte 2
-
-.cseg
-
-.macro i2c_sda_in
+;******************************************************************************
+; Marcros
+;******************************************************************************
+.macro I2C_SDA_INPUT
     cbi DDRB,I2C_SDA
 .endm
 
-.macro i2c_sda_out
+.macro I2C_SDA_OUTPUT
     sbi DDRB,I2C_SDA
 .endm
+
+;******************************************************************************
+; Program segment
+;******************************************************************************
+.cseg
 
 ;******************************************************************************
 ; Function...: i2c_init
@@ -37,247 +59,239 @@ heartbeat: .byte 2
 ; In.........: Nothing
 ; Out........: Nothing
 i2c_init:
-    ; Setup heartbeat counter
-    ldi r16,255
-    sts heartbeat,r16
-    ldi r16,10
-    sts heartbeat+1,r16
-
-    ; Set PORTA as output, and pin level = 0
-    ldi r16,0xff
-    out DDRA,r16
-    ldi r16,0
-    out PORTA,r16
-    
-    clr r16
-    sts i2c_state,r16
-
-    ; Preload Data Register
-    ldi r16, 0xff
-    out USIDR,r16
-
-    ; CLK and SDA as output
-    sbi DDRB,I2C_CLK
-    sbi DDRB,I2C_SDA
-
-    ; Set CLK = 0 and SDA = 0
+    ; CLK & SDA = low
     cbi PORTB,I2C_CLK
     cbi PORTB,I2C_SDA
 
-    ; Set CLK and SDA as input
+    ; CLK & SDA as input
     cbi DDRB,I2C_CLK
     cbi DDRB,I2C_SDA
 
-    ; Setup USI control register
-    ldi r16, 0b00101000             ; Set Two-Wire mode
+    ; Setup USI control & status register
+    ldi r16, I2C_CR_IDLE
     out USICR,r16
-
-    ; Clear status register flags and set counter for 8 bits
-    ldi r16, 0xf0
+    ldi r16,I2C_SR_BYTE
     out USISR,r16
     
-    ret
-
-i2c_heartbeat:
-    lds r16,heartbeat
-    cpi r16,0
-    brne i2c_heartbeat2
-    lds r17,heartbeat+1
-    cpi r17,0
-    breq i2c_heartbeat3
-    dec r17
-    sts heartbeat+1,r17
-
-i2c_heartbeat2:
-    dec r16
-    sts heartbeat,r16
-    ret
-
-i2c_heartbeat3:
-    in r16,PORTA
-    ldi r17,1
-    eor r16,r17
-    out PORTA,r16
-
-    ldi r16,255
-    sts heartbeat,r16
-    ldi r16,40
-    sts heartbeat+1,r16
     ret
 
 ;******************************************************************************
-; Function...: i2c_listen
-; Description: Listen for incoming I2C transmissions
+; Function...: i2c_isr_start
+; Description: Interrupt handler for start condition
 ; In.........: Nothing
 ; Out........: Nothing
-i2c_listen:
-    rcall i2c_heartbeat             ; Toggle PORTA pin 1 just to see we're alive
-
-    in r16,USISR                    ; Get USI Status register
+i2c_isr_start:
+    ; Set next state
+    ldi r16,STATE_CHECK_ADDRESS
+    sts i2c_state,r16
     
-    ldi r17,i2c_state               ; Load r17 with i2c_state
-    cpi r17,I2C_IDLE                ; Check if state is idle
-    brne i2c_listen2                ; If not jump to i2c_listen2
+    ; SDA as input
+    cbi DDRB,I2C_SDA
 
-    sbrc r16,USISIF                 ; Skip next line if Start Condition Flag is cleared
-    rjmp i2c_start                  ; Start Condition Flag is set, jump to i2c_start
-    rjmp i2c_listen                 ; Start over
+i2c_isr_start2:
+    ; Wait for CLK low (=start condition) or SDA high (=stop condition)
+    in r16,PINB
+    andi r16,(1<<I2C_CLK) | (1<<I2C_SDA)
+    cpi r16, (1<<I2C_CLK) | (0<<I2C_SDA)
+    breq i2c_isr_start2
 
-i2c_listen2:
-    sbrc r16, USIOIF                ; Skip next line if Counter Overflow Flag is cleared
-    rjmp i2c_overflow               ; Counter Overflow Flag is set, jump to i2c_overflow
-    rjmp i2c_listen                 ; Start over
+    ; Start or stop?
+    ldi r16,I2C_CR_ACTIVE       ; Default control value, for start condition
+    sbrc r16,I2C_SDA            ; Skip next line if SDA is low (start condition)
+    ldi r16,I2C_CR_STOP         ; SDA was set, load control value for stop condition
+    out USICR,r16               ; Write to control register
 
-i2c_start:
-    i2c_sda_in                      ; Set SDA as input
-
-    in r16,PORTA                    ; Start flag set
-    ori r16,2
-    out PORTA,r16
-
-i2c_start_wait:
-    in r16,PINB                     ; Get pin values
-    andi r16,0b00000101             ; Clear all but SDA and CLK
-    cpi r16, 0b00000100             ; Check if CLK=1 and SDA=0
-    breq i2c_start_wait             ; Keep waiting
+    ; Clear flags and counter
+    ldi r16,I2C_SR_BYTE
+    out USISR,r16
     
-    sbrc r16, I2C_SDA               ; Skip next line if SDA = 0
-    rjmp i2c_stop                   ; SDA was 1 => stop condition
+    reti
 
-    ldi r16,I2C_CHECK_ADDRESS       ; We have a start condition, set state to check address
+;******************************************************************************
+; Function...: i2c_isr_overflow
+; Description: Interrupt handler for USI shift register overflow
+; In.........: Nothing
+; Out........: Nothing
+i2c_isr_overflow:
+; Get current state
+;--------------------------------------------
+    lds r16,i2c_state
+
+    cpi r16,STATE_CHECK_ADDRESS
+    brne i2c_receive_byte
+
+; Check address
+;--------------------------------------------
+    in r16,USIDR                        ; Read address + R/W bit put on the bus by the master
+    mov r17,r16
+
+    lsr r16                             ; Right shift value and discard R/W bit
+    cpi r16,I2C_SLAVE_ADDRESS           ; Compare address
+    breq i2c_address_match
+    rjmp i2c_restart                    ; Not matching address => restart
+
+i2c_address_match:
+    sbi PORTA,0
+    ; Store data direction
+    sts i2c_ddr,r17
+
+    ; Set next state
+    ldi r16,STATE_WAIT_SLAVE_ACK
     sts i2c_state,r16
 
-    ldi r16,0b01101000
+    ; Clear internal offset if master write
+    clr r16
+    sbrs r17,0                          ; Skip next line if master read
+    sts i2c_offset,r16                  ; It was master write => set internal offset to 0
+
+    ; Send ACK
+i2c_send_bit:
+    out USIDR,r16                       ; r16 was cleared above
+    I2C_SDA_OUTPUT
+    sbi PORTB,I2C_SDA
+    ldi r16,I2C_SR_BIT
+    out USISR,r16
+    reti
+
+; Receive data
+;--------------------------------------------
+i2c_receive_byte:
+    cpi r16,STATE_RECEIVE_BYTE
+    brne i2c_wait_slave_ack
+    
+    sbi PORTA,1
+    ; Next state
+    ldi r16,STATE_WAIT_SLAVE_ACK
+    sts i2c_state,r16
+
+    ; Get data
+    in r16,USIDR
+
+    ; Is data new offset?
+    lds r17,i2c_offset
+    cpi r17,0
+    breq i2c_set_offset                 ; Yes it is
+    
+    ; Handle echo offset
+    lds r17,i2c_offset
+    cpi r17,0x80
+    brne i2c_send_ack
+    sts i2c_echo_buf,r16
+
+i2c_send_ack:
+    clr r16
+    rjmp i2c_send_bit
+
+i2c_set_offset:
+    sts i2c_offset,r16
+    rjmp i2c_send_ack
+
+; Wait for slave ack
+;--------------------------------------------
+i2c_wait_slave_ack:
+    cpi r16,STATE_WAIT_SLAVE_ACK
+    brne i2c_transmit_byte
+
+    sbi PORTA,2
+    ; Set SDA as input
+    I2C_SDA_INPUT
+    cbi PORTB,I2C_SDA
+
+    ; If master read, jump to transmit byte
+    lds r16,i2c_ddr
+    sbrc r16,0
+    rjmp i2c_transmit_byte2
+
+    ; It was master write, set next state
+    ldi r16,STATE_RECEIVE_BYTE
+    sts i2c_state,r16
+    
+    ; Configure to read a byte
+    ldi r16,I2C_SR_BYTE
+    out USISR,r16
+    reti
+
+; Transmit byte
+;--------------------------------------------
+i2c_transmit_byte:
+    cpi r16,STATE_TRANSMIT_BYTE
+    brne i2c_prep_master_ack
+
+    ; Set next state
+    sbi PORTA,3
+i2c_transmit_byte2:
+    ldi r16,STATE_PREP_MASTER_ACK
+    sts i2c_state,r16
+
+    ; Check offset
+    ldi r17,0                           ; Default return value
+
+    lds r16,i2c_offset
+    cpi r16,0x80
+    brne i2c_notexist
+    lds r17,i2c_echo_buf
+
+i2c_notexist:
+    ; Put value in buffer
+    out USIDR,r17
+
+    ; SDA as output
+    I2C_SDA_OUTPUT
+
+    ; Set SDA = high
+    sbi PORTB,I2C_SDA
+
+    ; Configure to send one byte
+    ldi r16,0b01110000
+    out USISR,r16
+    reti
+
+; Prepare to receive master ack
+;--------------------------------------------
+i2c_prep_master_ack:
+    cpi r16,STATE_PREP_MASTER_ACK
+    brne i2c_wait_master_ack
+
+    sbi PORTA,4
+    ; SDA as input & low
+    cbi PORTB,I2C_SDA
+    I2C_SDA_INPUT
+    
+    ; Set next state
+    ldi r16,STATE_WAIT_MASTER_ACK
+    sts i2c_state,r16
+
+    ; Configure to receive one bit
+    ldi r16,I2C_SR_BIT
+    out USISR,r16
+    reti
+
+; Wait for master ack
+;--------------------------------------------
+i2c_wait_master_ack:
+    cpi r16,STATE_WAIT_MASTER_ACK
+    brne i2c_restart
+
+    sbi PORTA,5
+    in r16,USIDR
+    sbrc r16,0
+    rjmp i2c_restart
+
+    ldi r16,STATE_TRANSMIT_BYTE
+    sts i2c_state,r16
+    ldi r16,I2C_SR_BYTE
+    out USISR,r16
+    reti
+
+; Restart
+;--------------------------------------------
+i2c_restart:
+    sbi PORTA,6
+    I2C_SDA_INPUT
+    cbi PORTB,I2C_SDA
+
+    ldi r16, I2C_CR_IDLE
     out USICR,r16
-
-    ldi r16,0xf0                    ; Clear Start Condition and Overflow Flag by writing a 1
+    ldi r16,I2C_SR_BYTE
     out USISR,r16
-
-    rjmp i2c_listen                 ; Start over
-
-i2c_stop:
-    ldi r16,I2C_IDLE                ; Set state = idle
-    sts i2c_state,r16
-    i2c_sda_in                      ; Set SDA as input
-    ldi r16,0xf0                    ; Clear Start Condition and Overflow Flag
-    out USISR,r16
-    rjmp i2c_listen                 ; Start over
-
-i2c_overflow:
-    lds r16,i2c_state               ; Load r16 with state
-    cpi r16,I2C_CHECK_ADDRESS       ; Is it check address?
-    brne i2c_overflow2              ; No...
-
-    ldi r16,I2C_ADDRESS_ACK         ; Yes. Next state is Address ACK
-    sts i2c_state,r16
-
-    sts i2c_addr,r17                ; Store address in RAM location i2c_addr
-
-    clr r16                         ; Clear r16
-    out USIDR,r16                   ; Put 0 = ACK in the Data Register
-
-    i2c_sda_out                     ; Set SDA as output
-
-    ldi r16,0xf0 | 0x0e             ; Clear Start Condition Flag and Overflow Flag + Set overflow counter to 0x0e => Send 1 bit
-    out USISR,r16
-    
-    rjmp i2c_listen                 ; Start over
-    
-i2c_overflow2:
-    cpi r16,I2C_ADDRESS_ACK         ; Is state = Address ACK?
-    brne i2c_overflow3              ; No...
-    
-    lds r16,i2c_addr                ; Load r16 from RAM location i2c_addr
-    sbrs r16,0                      ; Skip next line if bit 0 is set = Master writes to Slave
-    rjmp i2c_overflow2a             ; Bit 0 was clear = Master reads from slave, jump to overflow2a
-    
-    ldi r16,I2C_RECEIVE             ; Bit 1 was set = Master writes to slave
-    sts i2c_state,r16               ; Set that state
-
-    i2c_sda_in                      ; Set SDA as input
-
-    ldi r16,0xf0                    ; Clear Start Condition and Overflow Flag
-    out USISR,r16
-
-    rjmp i2c_listen                 ; Start over
-
-i2c_overflow2a:
-    ldi r16,I2C_SEND                ; Set state
-    sts i2c_state,r16
-
-    i2c_sda_out                     ; SDA as output
-
-    ldi r16,0x40                    ; Clear overflow flag
-    out USIDR,r16
-
-    ldi r16,0xf0                    ; Clear start condition and overflow flag
-    out USISR,r16
-
-    rjmp i2c_listen                 ; Start over
-
-i2c_overflow3:
-    cpi r16,I2C_RECEIVE
-    brne i2c_overflow4
-
-    ldi r16,I2C_RECEIVE_ACK
-    sts i2c_state,r16
-
-    i2c_sda_out
-
-    clr r16
-    out USIDR,r16
-
-    ldi r16,0xf0+0x0e
-    out USISR,r16
-
-    rjmp i2c_listen
-
-i2c_overflow4:
-    cpi r16,I2C_RECEIVE_ACK
-    brne i2c_overflow5
-
-    ldi r16, I2C_RECEIVE
-    sts i2c_state,r16
-
-    i2c_sda_in
-
-    ldi r16,0xf0
-    out USISR,r16
-    rjmp i2c_listen
-
-i2c_overflow5:
-    cpi r16,I2C_SEND
-    brne i2c_overflow6
-
-    ldi r16,I2C_SEND_WAIT
-    sts i2c_state,r16
-
-    i2c_sda_in
-
-    ldi r16, 0xf0+0x0e
-    out USISR,r16
-    
-    rjmp i2c_listen
-
-i2c_overflow6:
-    cpi r16,I2C_SEND_WAIT
-    brne i2c_overflow7
-
-    ldi r16,I2C_SEND
-    sts i2c_state,r16
-
-    i2c_sda_in
-
-    clr r16
-    sts i2c_state,r16
-
-    ldi r16,0xf0
-    out USISR,r16
-
-    rjmp i2c_listen
-
-i2c_overflow7:
-    i2c_sda_in
-    ldi r16,0xf0
-    out USISR,r16
-    rjmp i2c_listen
+    reti
--- main.asm
+++ main.asm
@@ -1,27 +1,39 @@
 .include "tn861def.inc"
 
-.org 0x0000
-
-.equ BOOTLOADER_ADDR = 0x00
-.org BOOTLOADER_ADDR
-
-.dseg
-
-status: .byte 1
-counter: .byte 2
-
 .cseg
 
-    rjmp main
+    rjmp main               ; 1. Reset
+    reti                    ; 2.
+    reti                    ; 3.
+    reti                    ; 4.
+    reti                    ; 5. 
+    reti                    ; 6.
+    reti                    ; 7.
+    rjmp i2c_isr_start      ; 8. USI Start
+    rjmp i2c_isr_overflow   ; 9. USI Overflow
+    reti                    
+    reti
+    reti
+    reti
+    reti
+    reti
+    reti
+    reti
+    reti
+    reti
+    reti
 
 main:
-    rcall i2c_init
-    rcall i2c_listen
+    cli
+    ldi r16,0xff
+    out DDRA,r16
+    ldi r16,0
+    out PORTA,r16
 
-    ;rcall fmw_init
-    ;rcall i2c_init
+    rcall i2c_init
+    sei
+
+loop:
+    rjmp loop
 
 .include "i2c.asm"
-;.include "fmw.asm"
-;.include "test.asm"
-;.include "usart.asm"
