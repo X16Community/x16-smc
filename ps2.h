f7814a78c74469ea00f426a79157472a8f4241cc
stefan-b-jakobsson <70063525+stefan-b-jakobsson@users.noreply.github.com>
2023-05-15 19:57:49 +0300

    Fix key issues, continued (#8)
    * Use Stefan's compact version of OneButton.
    
    * Enable Perixx keyboard; set numlock at power on
    
    * Remove activity LED debug code.
    
    * Move mouse and keyboard initialization state machine to START_RESET after reset.
    
    * Reset mouse PS2 state machine when powering off.
    
    * Pull x16community/main
    
    * Fix to ignore E012 E059 'decorators'
    
    * Fix to parse Print Screen
    
    * Another go at fixing extended key issue
    
    * Added support for End, Left GUI and Right GUI keys
    
    ---------
    
    Co-authored-by: jburks <joe@burks-family.us>

--- ps2.h
+++ ps2.h
@@ -392,6 +392,12 @@ class PS2KeyboardPort : public PS2Port<clkPin, datPin, size>
           return 62;
         case 0x14:  // Right Ctrl
           return 64;
+        case 0x1f:  // Left GUI
+          return 59;
+        case 0x27:  // Right GUI
+          return 63;
+        case 0x69:  // End
+          return 81;
         case 0x70:  // Insert
           return 75;
         case 0x71:  // Delete
@@ -493,8 +499,10 @@ class PS2KeyboardPort : public PS2Port<clkPin, datPin, size>
         case 0x21:    // After 0xe0 (extended code)
           // Update state
           if (value == 0xf0) scancode_state = 0x32; // Extended break code
-          else if (value != 0x12 && value != 0x59) {
-            if (!buffer_overrun) bufferAdd(ps2ext_to_keycode(value));
+          else {
+            if (value != 0x12 && value != 0x59) {
+              if (!buffer_overrun) bufferAdd(ps2ext_to_keycode(value));
+            }
             scancode_state = 0x00;
           }
 
