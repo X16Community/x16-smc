e2d6b0be5107dc09272385bb0524c2d6909e4ebb
stefan-b-jakobsson <stefan.b.jakobsson@gmail.com>
2024-01-14 08:29:17 +0200

    Remove unnecessary definitions

--- setup_ps2.h
+++ setup_ps2.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#define PS2_BAT_OK                      0xaa
-#define PS2_BAT_FAIL                    0xfc
-#define PS2_ACK                         0xfa
-
 void mouseTick();
 void mouseReset();
 void mouseSetRequestedId(uint8_t);
