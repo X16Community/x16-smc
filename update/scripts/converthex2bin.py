07b19803495e1781eed3babc4e337a36455bfb92
stefan-b-jakobsson <stefan.b.jakobsson@gmail.com>
2024-07-21 12:22:01 +0300

    Script paths, 3rd take

--- update/scripts/converthex2bin.py
+++ update/scripts/converthex2bin.py
@@ -2,4 +2,4 @@ from intelhex import IntelHex
 
 fw = IntelHex()
 fw.loadfile("../build/x16-smc.ino.hex", format="hex")
-fw.tofile("../resources/x16-smc.ino.bin", format="bin")
+fw.tofile("resources/x16-smc.ino.bin", format="bin")
